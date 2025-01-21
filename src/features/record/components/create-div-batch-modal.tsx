c556db38f824ee3be7754a857f7df64013aa7515
xc-6 <xc-6@users.noreply.github.com>
2025-01-20 19:52:47 -0600

    fix the text

--- src/features/record/components/create-div-batch-modal.tsx
+++ src/features/record/components/create-div-batch-modal.tsx
@@ -325,7 +325,7 @@ export const CreateDivBatchModal = () => {
                         <Input
                           disabled={isLoading}
                           className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
-                          placeholder="Enter Sold Price"
+                          placeholder="Enter dividend per share"
                           {...field}
                         />
                       </FormControl>
